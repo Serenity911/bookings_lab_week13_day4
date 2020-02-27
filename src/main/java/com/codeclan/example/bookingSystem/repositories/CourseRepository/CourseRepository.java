f112b50ff95ab92c21dc0faad4dbbec87df6efee
Steve Vance <steve.vance@hotmail.co.uk>
2020-02-27 13:53:51 +0000

    find bookings by date

--- src/main/java/com/codeclan/example/bookingSystem/repositories/CourseRepository/CourseRepository.java
+++ src/main/java/com/codeclan/example/bookingSystem/repositories/CourseRepository/CourseRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface CourseRepository extends JpaRepository<Course, Long>, CourseRepositoryCustom {
     List<Course> findByCourseRating(int courseRating);
 
+
 }
--- src/main/java/com/codeclan/example/bookingSystem/repositories/CustomerRepository/CustomerRepository.java
+++ src/main/java/com/codeclan/example/bookingSystem/repositories/CustomerRepository/CustomerRepository.java
@@ -5,6 +5,8 @@ import com.codeclan.example.bookingSystem.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface CustomerRepository extends JpaRepository<Customer, Long>, CustomerRepositoryCustom {
 }
